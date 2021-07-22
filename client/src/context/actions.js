3cea15bf0aaa11cb81dc90baa45e8a43f2cc4fad
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2021-07-22 08:02:13 -0500

    cleanup

--- client/src/context/actions.js
+++ client/src/context/actions.js
@@ -1,107 +1,104 @@
 //import libraries
-import axios from 'axios'
+import axios from "axios";
 
 //import token
-import setAuthToken from "../utils/setAuthToken"
+import setAuthToken from "../utils/setAuthToken";
 
-export const register = async(dispatch, formData) =>{
-    const config = {
-        headers: {
-            "Content-Type":"application/json",
-        },
-    };
+export const register = async (dispatch, formData) => {
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
 
-    try {
-        const res = await axios.post("/api/users", formData, config)
-        localStorage.setItem("token", res.data.token)
-        dispatch({
-            type: "REGISTER",
-            payload: res.data
-        })
-    } catch (error) {
-        console.log(error)
-        dispatch({
-            type: "REGISTER_FAIL", 
-            payload: error.response.data.msg,
-        })
-    }
-}
+  try {
+    const res = await axios.post("/api/users", formData, config);
+    localStorage.setItem("token", res.data.token);
+    dispatch({
+      type: "REGISTER",
+      payload: res.data,
+    });
+  } catch (err) {
+    console.log(err);
+    dispatch({
+      type: "REGISTER_FAIL",
+      payload: err.response.data.msg,
+    });
+  }
+};
 
-export const login = async(dispatch, formData) =>{
-    const config = {
-        headers: {
-            "Content-Type":"application/json",
-        },
-    };
+export const login = async (dispatch, formData) => {
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
 
-    try {
-        const res = await axios.post("/api/auth", formData, config)
-        localStorage.setItem("token", res.data.token)
+  try {
+    const res = await axios.post("/api/auth", formData, config);
+    localStorage.setItem("token", res.data.token);
+    dispatch({
+      type: "REGISTER",
+      payload: res.data,
+    });
+  } catch (err) {
+    console.log(err);
+    dispatch({
+      type: "REGISTER_FAIL",
+      payload: err.response.data.msg,
+    });
+  }
+};
 
-        dispatch({
-            type:"REGISTER",
-            payload: res.data, 
-        })
-        
-    } catch (error) {
-        console.log(error)
-        dispatch({
-            type: "REGISTER_FAIL", 
-            payload: error.response.data.msg,
-        })
-    }
-}
-
-export const loadUser = async(dispatch) => {
-    if(localStorage.token){
-        setAuthToken(localStorage.token)
-    }
-
-    try {
-        const res = await axios.get('/api/auth')
-
-        dispatch({type: "LOAD_USER", payload: res.data})
-    } catch (error) {
-        dispatch({type: "LOGIN_FAIL", payload: error.response.data.msg})
-    }
-}
+export const loadUser = async (dispatch) => {
+  if (localStorage.token) {
+    setAuthToken(localStorage.token);
+  }
+  try {
+    const res = await axios.get("/api/auth");
+    dispatch({ type: "LOAD_USER", payload: res.data });
+  } catch (err) {
+    dispatch({ type: "LOGIN_FAIL", payload: err.response.data.msg });
+  }
+};
 
 export const addTodo = async (dispatch, formData) => {
-    const config = {
-        headers: {
-            "Content-Type":"application/json",
-        },
-    };
-    
-    try {
-        axios.post('/api/todos', formData, config)
-    } catch (error) {
-        console.log(error.response.data.msg)
-    }
-}
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+  try {
+    axios.post("/api/todos", formData, config);
+    loadTodos(dispatch);
+  } catch (err) {
+    console.log(err.response.data.msg);
+  }
+};
 
 export const loadTodos = async (dispatch) => {
-    try {
-      const res = await axios.get('/api/todos')  
-      dispatch({type: "LOAD_TODOS", payload: res.data})
-    } catch (error) {
-        console.log(error)
-    }
-}
+  try {
+    const res = await axios.get("/api/todos");
+    dispatch({ type: "LOAD_TODOS", payload: res.data });
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 export const deleteTodo = async (dispatch, id) => {
-    try {
-        await axios.delete(`/api/todos/${id}`)
-        loadTodos(dispatch)
-    } catch (error) {
-        console.log(error)
-    }
-}
+  try {
+    await axios.delete(`/api/todos/${id}`);
+    loadTodos(dispatch);
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 export const updateTodo = async (dispatch, id, formData) => {
-    try {
-        await axios.put(`/api/todos/${id}`, formData)
-    } catch (error) {
-        console.log(error)
-    }
-}
+  try {
+    await axios.put(`/api/todos/${id}`, formData);
+    loadTodos(dispatch);
+  } catch (err) {
+    console.log(err);
+  }
+};
